5e8e15278f0751bfdb3c9ac46ae09962a97e7b6f
abusayedwd <abusayednalita1234@gmai.com>
2025-09-03 16:13:13 +0600

    fixr

--- api/index.js
+++ api/index.js
@@ -1,13 +1,13 @@
-const serverless = require("serverless-http");
-const { app, connectToDatabase } = require("../src/index");
+// const serverless = require("serverless-http");
+// const { app, connectToDatabase } = require("../src/index");
 
-module.exports = async (req, res) => {
-  try {
-    await connectToDatabase();
-    const handler = serverless(app);
-    return handler(req, res);
-  } catch (err) {
-    console.error("Serverless error:", err);
-    res.status(500).json({ error: "Internal Server Errorree" });
-  }
-};
+// module.exports = async (req, res) => {
+//   try {
+//     await connectToDatabase();
+//     const handler = serverless(app);
+//     return handler(req, res);
+//   } catch (err) {
+//     console.error("Serverless error:", err);
+//     res.status(500).json({ error: "Internal Server Errorree" });
+//   }
+// }; 
