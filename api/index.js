e00f4bcd7282c168295681bdeb54ded4d7159edd
abusayedwd <abusayednalita1234@gmai.com>
2025-09-04 11:09:23 +0600

    fixr

--- api/index.js
+++ api/index.js
@@ -1,18 +1,18 @@
-// const { connectToDatabase } = require("../src/index");
-// const { app } = require("../src/app");
+const { connectToDatabase } = require("../src/index");
+const { app } = require("../src/app");
 
-// // Ensure this is the default export for Vercel
-// export default async function handler(req, res) {
-//   try {
-//     // Connect to database
-//     await connectToDatabase();
+// Ensure this is the default export for Vercel
+export default async function handler(req, res) {
+  try {
+    // Connect to database
+    await connectToDatabase();
     
-//     // If your app is an Express app, you can use it directly
-//     return app(req, res);
-//   } catch (error) {
-//     console.error("Handler error:", error);
-//     res.status(500).json({ 
-//       error: "Internal Server Error" 
-//     });
-//   }
-// }
+    // If your app is an Express app, you can use it directly
+    return app(req, res);
+  } catch (error) {
+    console.error("Handler error:", error);
+    res.status(500).json({ 
+      error: "Internal Server Error" 
+    });
+  }
+}
